b3550b2c415f915fb68cf585f4b3127599e8f38b
Nikita Dugar <nikita@clouddrove.com>
2019-09-11 18:39:38 +0530

    add terratest

--- _example/outputs.tf
+++ _example/outputs.tf
@@ -1,5 +1,9 @@
-
 output "sftp" {
   value       = module.sftp.*.id
   description = "The Server ID of the Transfer Server (e.g. s-12345678)"
+}
+
+output "tags" {
+  value       = module.sftp.tags
+  description = "A mapping of tags to assign to the SFTP."
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 #Module      : SFTP
 #Description : Terraform sftp module variables.
 
-output "transfer_server_id" {
+output "id" {
   value       = aws_transfer_server.transfer_server.*.id[0]
   description = "The Server ID of the Transfer Server (e.g. s-12345678)."
 }
@@ -9,4 +9,9 @@ output "transfer_server_id" {
 output "transfer_server_endpoint" {
   value       = aws_transfer_server.transfer_server.*.endpoint[0]
   description = "The endpoint of the Transfer Server (e.g. s-12345678.server.transfer.REGION.amazonaws.com)."
+}
+
+output "tags" {
+  value       = module.labels.tags
+  description = "A mapping of tags to assign to the resource."
 }
