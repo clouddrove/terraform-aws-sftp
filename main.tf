5d25bfdd8fe38054b8c2f40d7d3dd82e827ee5af
himanshu-uniyal <himanshu@clouddrove.com>
2020-10-21 19:37:05 +0530

    added ssh key

--- main.tf
+++ main.tf
@@ -93,5 +93,5 @@ resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
   count     = var.enable_sftp ? 1 : 0
   server_id = var.endpoint_type == "VPC" ? aws_transfer_server.transfer_server_vpc.*.id[0] : aws_transfer_server.transfer_server.*.id[0]
   user_name = aws_transfer_user.transfer_server_user.*.user_name[0]
-  body      = file(var.key_path)
+  body      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 example@example.com"
 }
