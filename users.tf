ad21e8ca3863a08f1ffac4f59915eaad8325af6b
Callum <callumccr@gmail.com>
2022-07-19 06:53:28 +0100

    updated iam user permission

--- users.tf
+++ users.tf
@@ -27,6 +27,35 @@ resource "aws_iam_role_policy" "user" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Sid":"DenyMkdir",
+      "Action":[
+          "s3:PutObject"
+      ],
+      "Effect":"Deny",
+      "Resource":"arn:aws:s3:::${module.s3.s3_buckets[module.this.name].bucket_id}/*/"
+    }
+    {
+      "Sid":"ReadWriteS3",
+      "Action": [
+            "s3:ListBucket"
+                ],
+      "Effect": "Allow",
+      "Resource": ["arn:aws:s3:::${module.s3.s3_buckets[module.this.name].bucket_id}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion",
+        "s3:GetObjectVersion",
+        "s3:GetObjectACL",
+        "s3:PutObjectACL"
+      ],
+      "Resource": ["arn:aws:s3:::${module.s3.s3_buckets[module.this.name].bucket_id}/*"]
+    }
+    {
         "Sid": "AllowListingOfUserFolder",
         "Action": [
             "s3:ListBucket"
