fa3d6c9df383debee5e1fc51e7f6f9aa36a7fe8c
Callum <callumccr@gmail.com>
2022-07-19 09:22:24 +0100

    fix: removed uneccessary permissions

--- users.tf
+++ users.tf
@@ -66,6 +66,8 @@ resource "aws_iam_role_policy" "user" {
       "Action": [
         "s3:PutObject",
         "s3:GetObject",
+        "s3:DeleteObjectVersion",
+        "s3:DeleteObject",
         "s3:GetObjectVersion"
       ],
     "Resource" : [
