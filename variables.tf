9b1cc04c7d88f1a3500d60e921bc794eef1a63de
theprashantyadav <theprashantyadav7@gmail.com>
2023-08-03 16:42:20 +0530

    feat: update new github-action and subnet vpc tag

--- variables.tf
+++ variables.tf
@@ -71,7 +71,7 @@ variable "s3_bucket_id" {
 
 variable "key_path" {
   type        = string
-  default     = ""
+  default     = null
   description = "Name  (e.g. `~/.ssh/id_rsa.pub`)."
   sensitive   = true
 }
